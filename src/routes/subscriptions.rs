17b417234d12231410fe0ca0790c1c3f2d0c34d0
Angelo Bongiorno <s331164@studenti.polito.it>
2025-11-01 17:53:27 +0100

    end of 4.4

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -14,7 +14,9 @@ pub async fn subscribe(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>
 ) -> HttpResponse {
-
+    let request_id = Uuid::new_v4();
+    log::info!("Request id: {} - adding {}, {} as new subscriber!", request_id, form.email, form.name);
+    log::info!("Request id: {} - saving new subscriber to the db!", request_id);
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at_timestamp)
@@ -28,9 +30,12 @@ pub async fn subscribe(
     .execute(pool.get_ref())
     .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            log::info!("Request id: {} - new subscriber was saved!", request_id);
+            HttpResponse::Ok().finish()
+        },
         Err(e) => {
-            println!("Failed to execute query: {}", e);
+            log::info!("Request id: {} - Failed to execute query: {:?}", request_id, e);
             HttpResponse::InternalServerError().finish()
         }
     }
